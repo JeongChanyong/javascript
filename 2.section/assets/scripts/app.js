886d2fb806d8b54bfc89368b8ffddae3c35adc10
DESKTOP-0I3K5MH\chanyong <Chanyong.Jeong@kakao.com>
2024-03-06 23:47:09 +0900

    section 3 진행중

--- 2.section/assets/scripts/app.js
+++ 2.section/assets/scripts/app.js
@@ -23,33 +23,39 @@ function writeToLog(operationIdentifier, prevResult, operationNumber, newResult)
 
 function calculateResult(calculationType) {
 
-    if (calculationType !== 'ADD' && calculationType !== 'SUBTRACT' && calculationType !== 'MULTIPLY' && calculationType !== 'DIVIDE') {
+    const enterNumber = getUserNumberInput();
+
+    // 앞에 조건이 true 이면 다음 조건으로 이동 하여 비교 한다. 모든 조건에 true 가 나오게 되면 31번째 줄로 이동 하지 않는다.
+    // OR 연산자를 이용해 앞에 && 조건과는 별개로 enterNumber 가 0 이면 참값을 반환해 실행 시키지 않는다.
+    // enterNumber 값이 0 이면 false 인데 ! 이용해서 Falsy 가 아니다가 되므로 참이 true 반환, 0 !== false 와 같은 표현
+    // TODO javascript Truthy and Falsy 찾아서 공부 하기
+    if (calculationType !== 'ADD' && calculationType !== 'SUBTRACT' && calculationType !== 'MULTIPLY' && calculationType !== 'DIVIDE' || !enterNumber) {
         return;
     }
-
-
-    const enterNumber = getUserNumberInput();
     const initialResult = currentResult;
     let mathOperator;
 
+    // OR의 경우 첫 번째 조건이 false 이면 다음조건을 확인 한다. 하나라도 true 면 해당 블럭의 로직을 실행 한다.
+    // if (calculationType === 'ADD' || calculationType === 'SUBTRACT' || calculationType === 'MULTIPLY' || calculationType === 'DIVIDE') {
 
+        if (calculationType === 'ADD') {
+            currentResult += enterNumber;
+            mathOperator = '+';
+        } else if (calculationType === 'SUBTRACT') {
+            currentResult -= enterNumber;
+            mathOperator = '-';
+        } else if (calculationType === 'MULTIPLY') {
+            currentResult *= enterNumber;
+            mathOperator = '*';
+        } else {
+            currentResult /= enterNumber;
+            mathOperator = '/';
+        }
 
-    if (calculationType === 'ADD') {
-        currentResult += enterNumber;
-        mathOperator = '+';
-    } else if (calculationType === 'SUBTRACT') {
-        currentResult -= enterNumber;
-        mathOperator = '-';
-    } else if (calculationType === 'MULTIPLY' ) {
-        currentResult *= enterNumber;
-        mathOperator = '*';
-    } else {
-        currentResult /= enterNumber;
-        mathOperator = '/';
-    }
+        createAndWriteLog(mathOperator, initialResult, enterNumber);
+        writeToLog(calculationType, initialResult, enterNumber, currentResult);
 
-    createAndWriteLog(mathOperator, initialResult, enterNumber);
-    writeToLog(calculationType, initialResult, enterNumber, currentResult);
+    // }
 }
 function add() {
     calculateResult('ADD')
---
+++ 3.section/assets/scripts/app.js
@@ -0,0 +1,90 @@
+// 전역 변수 값은 대문자로 해주는게 좋다.
+const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
+
+
+// 사용자와 몬스터의 최대 체력 (차후 사용자로 부터 입력 받기에 let 으로 설정)
+let chosenMaxLife = 100;
+
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
+
+// 체력 조정
+adjustHealthBars(chosenMaxLife);
+
+function endRound() {
+
+    const initialPlayerHealth = currentPlayerHealth;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+
+    if (currentPlayerHealth <= 0 && hasBonusLife) {
+        hasBonusLife = false;
+        removeBonusLife();
+        currentPlayerHealth = initialPlayerHealth;
+        setPlayerHealth(initialPlayerHealth)
+        alert('보너스 생명이 없으면 죽었습니다.')
+    }
+
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert("몬스터를 처치 하였습니다.");
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert("플레이어 사망");
+    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+        alert("무승부");
+    }
+    /**
+     * else 사용할 경우 이벤트 발생시 마다 해당 블록을 실행 하기에 해당 로직에서는 사용 하면 안된다.
+     */
+}
+
+function attackMonster(mode) {
+
+    let maxDamage;
+
+    if (mode === 'ATTACK') {
+        maxDamage = ATTACK_VALUE;
+    } else if (mode === 'STRONG_ATTACK') {
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+    const damage = dealMonsterDamage(maxDamage);
+    currentMonsterHealth -= damage;
+
+    endRound();
+}
+
+function attackHandler() {
+    attackMonster('ATTACK');
+}
+
+function strongAttackHandler() {
+    attackMonster('STRONG_ATTACK');
+}
+
+function healPlayerHandler() {
+
+    let healValue;
+
+    /**
+     * 처음 시작 시 currentPlayerHealth 100
+     * 연산자 우선 순위로 인해 >= 기준 우항 먼저 계산을 한다
+     * 초기값 100 - HEAL_VALUE (20) 의 값 80 보다 현재 currentPlayerHealth 값이 크다면
+     * 피가 채워져서는 안된다.
+     */
+    if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+        alert('피가 가득 입니다.')
+        healValue = chosenMaxLife - currentPlayerHealth
+    } else {
+        healValue = HEAL_VALUE;
+    }
+    increasePlayerHealth(healValue);
+    currentPlayerHealth += healValue;
+    endRound();
+}
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
